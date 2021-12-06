3566b497609e7a4bb39db497f1d9d939312a5968
zazu <z.biedzinska@gmail.com>
2021-12-06 16:32:52 +0000

    2c

--- index.js
+++ index.js
@@ -17,3 +17,23 @@ let myCollection = [
   ];
 
   console.log(myCollection);
+
+
+//   Create a function called `describeItem`, which should take in an item as a parameter (the argument handed to this function would be an item from our collection). The function should `console.log` a message according to how many of the item you have.
+
+// If there's only one of the item in your collection, it should log "I have a `name`. Here's what I like about it: `whatILike`". If you have more than one of it, the message should log "I have `count` `name`s. Here's what I like about them: `whatILike`".
+
+// For example, the result of calling the `describeItem` function with the first item in our collection would be:
+// _"I have a School of Code mug. Here's what I like about it: It has my cute pixel character on it!"_
+
+// Call your function below where you've defined it, handing in the first item in `myCollection`.
+
+// Now run the file again (using `node index.js`);
+
+function describeItem(){
+    myCollection.forEach(function(item){
+        console.log(`I have ${item.name} here's what i like about it: ${item.whatILike}`)
+    })
+}
+
+describeItem()
